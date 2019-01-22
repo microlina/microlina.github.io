52bb449208c2620b76ac60748c3b513859127976
microlina <catarina.gralha@gmail.com>
2019-01-22 16:49:23 +0000

    Removed the quality evaluation framework. Added the CV

--- quality_framework/tools/iStarLab2.0/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    istar.setupModel();
-    istar.setupDiagram();
-    istar.setupMetamodel(istar.metamodel);
-    ui.setupUi();
-    //istar.examples.loadPistarWelcome();
-    ui.selectModel();//clear selection
-});
